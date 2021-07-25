746d572f1b92c00831a1056ea45dbb1fe17f2f84
Caleb <Caleb@Calebs-MacBook-Air.local>
2021-07-25 12:45:14 -0400

    another one

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -30,7 +30,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Caleb Qi <Link to="/">qalbii.com</Link>.</p>
     </section>
   </section>
 );
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,25 +5,22 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+    description={"Caleb Qi's personal website. Hopeless idealist forced to compromise, "
+    + 'self loathing and self proclaimed artist'}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
-          <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
-          </p>
+          <h2 data-testid="heading"><Link to="/">Welcome 欢迎</Link></h2>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
+      <p> Welcome to my website. It was inspired and built on the backs of people who are way more creative and smart at website building than me. And while I
+      hate not being original, I mean, only truly great pieces of art happen to be original, right? Anyways, feel free to read more
+        <Link to="/about">about me</Link>, or check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
         view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
+        or <Link to="/contact">stalk me</Link>.
       </p>
     </article>
   </Main>
